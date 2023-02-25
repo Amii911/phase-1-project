a59171e671c9758b0fb6f4875badd3af7690d858
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-02-24 22:23:14 -0500

    add script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function renderNeeded(showObject) {
     console.log(showObject.name)
+    const ul = document.getElementById('beer-list')
+	const li = document.createElement('li')
+	li.innerText = showObject.name
+	li.addEventListener('click', () => displayBeer(showObject.name))
+	ul.append(li)
 }
 
+ const displayBeer = (breweries) => {
+    console.log(breweries)
+ }
 
 function getBeerList() {
     const getListUrl = "https://api.openbrewerydb.org/breweries"
@@ -12,4 +20,5 @@ function getBeerList() {
     })
     )
 }
+
 getBeerList()
