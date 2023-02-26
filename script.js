1637f3b3ce306484c8adccebe8cc464d52875267
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-02-25 19:31:08 -0500

    add script.js

--- script.js
+++ script.js
@@ -9,16 +9,16 @@
 
 function renderBreweryInfor(info){
     const divInfo = document.querySelector('#info')
-    let p = document.createElement('h1')
+    let p = document.createElement('p')
     p.innerTest = info.name
     divInfo.appendChild(p)
-    let p = document.createElement('p')
+    p = document.createElement('p')
     p.innerTest = info.street
     divInfo.appendChild(p)
-    let p = document.createElement('p')
+    p = document.createElement('p')
     p.innerTest = info.city
     divInfo.appendChild(p)
-    let p = document.createElement('p')
+    p = document.createElement('p')
     p.innerTest = info.state
     divInfo.appendChild(p)
 
