d39de5626f90c6802d4921b71475510a78ea25a9
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-02-25 22:28:07 -0500

    add script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const divInfo = document.querySelector('#info');
 function doRefresh() {
     ul.innerHTML = '';
     clearBreweryInfo();
-    document.querySelector('#txtCity').value = ''
+    document.querySelector('#txtCity').value = '';
 }
 
 
